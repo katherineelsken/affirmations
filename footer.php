0eca78a578dc354f3e5aca9dc308c2d03251b2bf
Katherine Elsken <katstar01@gmail.com>
2016-08-17 21:40:43 +0000

    First commit

---
+++ footer.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package Affirmations
+ */
+
+?>
+
+	</div><!-- #content -->
+
+	<footer id="colophon" class="site-footer" role="contentinfo">
+		<div class="site-info">
+			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'affirmations' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'affirmations' ), 'WordPress' ); ?></a>
+			<span class="sep"> | </span>
+			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'affirmations' ), 'affirmations', '<a href="http://www.katherineelsken.com" rel="designer">Katherine Elsken</a>' ); ?>
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
